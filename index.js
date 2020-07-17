d0216a982eb68defb0e04cc15ee5027196f7b1d8
Roman Senin <romansenin15@gmail.com>
2020-07-16 18:23:29 -0700

    created js file, added comments to outline goals

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+  - goal: let users select options, then show price
+  - handling the price:
+    - we can store it in a variable
+    - the price should change when an option is clicked
+  - handling the options:
+    - use checkboxes for options that can all be added 
+    - use radio buttons where user can only select one
+    - data can be stored in an object
+  - handling the display:
+    - we want to show a list of selections, not just price
+*/
